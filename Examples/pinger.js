d7b1b1ab619ca1ab4337c0b81bb3d21dc29b553e
invernizzi <fabrizio.invernizzi@telecomitalia.it>
2014-10-02 15:16:23 +0200

    Example dir updated.

--- Examples/pinger.js
+++ Examples/pinger.js
@@ -2,84 +2,153 @@
  * Pinger probe implementing the Capability push, Specification pull model
  *
  * @author fabrizio.invernizzi@telecomitalia.it
- * @version 0.0.1a
+ * @version 0.2.0
  *
  */
 
 var exec = require('child_process').exec,
-    mplane = require('mplane'),
-    _ = require("lodash"),
-    request = require("request"),
-    http = require("http"),
+    mplane = require('mplane')
+    ,supervisor = require("./mplane_http_transport"
+    , _ = require("lodash"),
     inquirer = require("inquirer"),
-    os=require('os'),
     url = require('url'),
-    prettyjson = require('prettyjson'),
-    async = require("async");
+    async = require("async")
+    ,fs = require('fs'));
 
-var ifaces=os.networkInterfaces();
-var ipAdresses = [];
-for (var dev in ifaces) {
-    var alias=0;
-    ifaces[dev].forEach(function(details){
-        if (details.family=='IPv4') {
-            //console.log(dev+(alias?':'+alias:''),details.address);
-            ipAdresses.push(details.address);
-            ++alias;
-        }
-    });
+var CONFIGFILE = "pinger.json"; //TODO:This should be overwrittable by cli
+
+//-----------------------------------------------------------------------------------------------------------
+// READ CONFIG
+var configuration;
+try {
+    configuration = JSON.parse(fs.readFileSync(CONFIGFILE));
 }
+catch (err) {
+    console.log('There has been an error parsing the configuration file.')
+    console.log(err);
+    process.exit();
+}
+//-----------------------------------------------------------------------------------------------------------
 
-var __MY_IP__ = "127.0.0.1";
-var supervisorIP = "127.0.0.1";
-var supervisorPort = 2426;
-var specificationLink = "";
+var __MY_IP__ = configuration.main.ipAdresses[0];
 
 var questions = [
     {
         type: "list",
         name: "ipSource",
         message: "Select your source IP address",
-        choices: ipAdresses,
+        choices: configuration.main.ipAdresses,
         filter: function( val ) { return val.toLowerCase(); }
     }
 ];
 
+var connected = false;
 prompt = inquirer.prompt( questions, function( answers ) {
+    var capability = [];
     __MY_IP__ = answers.ipSource;
 
     // Initialize available primitives from the registry
     mplane.Element.initialize_registry("registry.json");
 
-    // Source address and destination parameters initialized from the registry
-    var sourceAddress = new mplane.Element("source.ip");
-    //  My source address should never be changed!
-    sourceAddress.addConstraint( __MY_IP__ );
+    var pingerCapability = new mplane.Capability();
+    pingerCapability.set_when("now ... future / 1s");
+    pingerCapability.add_parameter({
+        type:"destination.ip4",
+        constraints:"192.168.0.1 ... 192.168.255.255"
+    }).add_parameter({
+        type:"number",
+        constraints:"1 ... 10"
+    }).add_parameter({
+            type:"source.ip4",
+            constraints:__MY_IP__
+    }).add_result_column("delay.twoway")
+        .set_metadata_value("System_type","Pinger")
+        .set_metadata_value("System_version","0.1a")
+        .set_metadata_value("System_ID","Lab test machine").update_token();
+    pingerCapability.set_label(configuration.main.pingerLabel);
 
-    var destinationAddress = new mplane.Element("destination");
-    destinationAddress.addConstraint("10.34.32.0 ... 10.34.32.255");
+    var traceCapability = new mplane.Capability();
+    traceCapability.set_when("now ... future / 1s");
+    traceCapability.add_parameter({
+        type:"destination.ip4",
+        constraints:"192.168.0.1 ... 192.168.255.255"
+    }).add_parameter({
+        type:"source.ip4",
+        constraints:__MY_IP__
+    }).add_result_column("delay.twoway").add_result_column("hops.ip")
+        .set_metadata_value("System_type","Tracer")
+        .set_metadata_value("System_version","0.1a")
+        .set_metadata_value("System_ID","Lab test machine").update_token();
+    traceCapability.set_label(configuration.main.tracerouteLabel);
 
-    var numberOfEchoRequests = new mplane.Element("number");
-    numberOfEchoRequests.addConstraint("1 ... 10");
+    capability.push(pingerCapability , traceCapability);
 
-    // Ping result - MEAN RTT
-    var meanRTT = new mplane.Element("delay.twoway");
-
-    var statement = new mplane.Statement({verb:mplane.Statement.VERB_MEASURE});
-    statement.add_parameter("My source address" , sourceAddress , __MY_IP__);
-
-    // Here we should set a valid ip address in accordance with the constraints on the destinationAddress
-    statement.add_parameter("The destination address" , destinationAddress , __MY_IP__);
-    statement.add_parameter("The number of echo requests the pinger should send" , numberOfEchoRequests , 1);
-    statement.add_result_column("Mean RTT", meanRTT);
-
-    // THE CAPABILITY we will announce
-    var capability = new mplane.Capability(statement);
-
-    registerToSupervisor(capability);
-    checkForSpecifications(10000);
+    pushCapPullSpec(capability);
+    var recheck = setInterval(function(){
+        if (!connected){
+            console.log("Supervisor unreachable. Retry in "+configuration.main.retryConnect/1000 + " seconds...");
+            pushCapPullSpec(capability);
+        }else{
+            console.log("------------------------------");
+            console.log("");
+            console.log("Checking for Specifications...");
+            console.log("");
+            console.log("------------------------------");
+            clearInterval(recheck);
+        }
+    } , configuration.main.retryConnect);
 });
 
+function pushCapPullSpec(capabilities){
+    console.log("***************************");
+    console.log("REGISTERING MY CAPABILITIES");
+    console.log("***************************\n");
+    supervisor.registerCapabilities(capabilities , {
+            host: configuration.supervisor.host
+            ,port: configuration.supervisor.port
+            ,caFile: configuration.ssl.ca
+            ,keyFile: configuration.ssl.key
+            ,certFile: configuration.ssl.cert
+        },function(err , data){
+            if (err){
+                return false;
+            }else{
+                supervisor.checkSpecifications(
+                    {
+                        host: configuration.supervisor.host
+                        ,port: configuration.supervisor.port
+                        ,caFile: configuration.ssl.ca
+                        ,keyFile: configuration.ssl.key
+                        ,certFile: configuration.ssl.cert
+                    }
+                    ,function(specification){
+                        var label = specification.get_label();
+                        console.log("------------------------------------------");
+                        console.log("So i should work...")
+                        // FIXME: this MUST be changed!!!
+                        specification.set_when("2014-09-29 10:19:26.765203 ... 2014-09-29 10:19:27.767020");
+                        if (label ==  configuration.main.pingerLabel){
+                            execPing( specification );
+                        }
+                        if (label ==  configuration.main.tracerouteLabel){
+                            execTraceroute(specification);
+                        }
+                    }, function(err){
+                        // For some reason we have no capability registered
+                        if (err.message == 428){
+                            pushCapPullSpec(capabilities);
+                        }
+                        else
+                            console.log(err);
+                    }
+                );
+                connected = true;
+                return true;
+            }
+        }
+    );
+
+}
 
 function mean(values){
     var sum = 0 , elements = 0;
@@ -90,6 +159,86 @@ function mean(values){
         }
     });
     return (sum/elements);
+}
+/**
+ *
+ * @param specification The mplane Specification
+ */
+function execPing(specification){
+    var dest = specification.get_parameter_value("destination.ip4");
+    var reqNum = specification.get_parameter_value("number");
+    async.waterfall([
+        function(callback){
+            doAPing(dest, 4 , reqNum , callback);
+        }
+    ], function (err, meanRTT) {
+        console.log("CALCULATED:"+meanRTT);
+        supervisor.registerResult(
+            specification
+            , {
+                host: configuration.supervisor.host
+                ,port: configuration.supervisor.port
+                ,caFile: configuration.ssl.ca
+                ,keyFile: configuration.ssl.key
+                ,certFile: configuration.ssl.cert
+            },{
+                "delay.twoway":meanRTT
+            }
+            ,function(err , data){
+                if (err)
+                    console.log(err);
+                else{
+                    console.log("------------------------------------------");
+                }
+            }
+        ); //supervisor.registerResult
+    }); //waterfall
+}
+
+/**
+ *
+ * @param specification The mplane Specification
+ */
+function execTraceroute(specification){
+    var dest = specification.get_parameter_value("destination.ip4");
+    async.waterfall([
+        function(callback){
+            console.log("Tracing to "+dest);
+            doATrace(dest , function (err,hops) {
+                if (err){
+                    callback(err , null);
+                }else{
+                    callback(null , hops);
+                }
+            });
+        }
+    ], function (err, hops) {
+        console.log("DONE");
+        if (err){
+            console.log(err);
+        }else{
+            supervisor.registerResult(
+                specification
+                , {
+                    host: configuration.supervisor.host
+                    ,port: configuration.supervisor.port
+                    ,caFile: configuration.ssl.ca
+                    ,keyFile: configuration.ssl.key
+                    ,certFile: configuration.ssl.cert
+                },{
+                    "delay.twoway":mean(hops),
+                    "hops.ip":hops.length
+                }
+                ,function(err , data){
+                    if (err)
+                        console.log(err);
+                    else{
+                        console.log("------------------------------------------");
+                    }
+                }
+            ); //supervisor.registerResult
+        }
+    }); //waterfall
 }
 
 function doAPing(destination , Wait , requests , callback){
@@ -116,8 +265,6 @@ function doAPing(destination , Wait , requests , callback){
         });
         console.log(mean(times))
         callback(null, mean(times));
-        //console.log("Mean RTT:"+mrtt);
-        //return mrtt;
     }
     if (error !== null) {
       callback(error , null);
@@ -125,137 +272,26 @@ function doAPing(destination , Wait , requests , callback){
   });
 }
 
-/**
- * POST to the supervisor all the probe information
- * @param capability
- */
-function registerToSupervisor( capability) {
-    // Serialize the capability Object
-    var post_data = capability.to_dict();
-
-    var post_options = {
-        path: '/register',
-        method: 'POST',
-        host: supervisorIP,
-        port: supervisorPort,
-        followRedirect:false,
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': post_data.length
-        }
-    };
-
-    // Set up the request
-    var post_req = http.request(post_options, function(res) {
-        // We expect a redirect to the Specification url
-        if (res.statusCode == 302){
-            console.log("The supervisor answered with:");
-            console.log(res.headers.location);
-            specificationLink = res.headers.location;
-        }else{
-            console.log("Unable to register to the supervisor. May be we are already registered?("+res.statusCode+")");
-        }
-    });
-
-    // post the data
-    post_req.write(post_data);
-    post_req.end();
-}
-
-function checkForSpecifications(period){
-    setInterval(function(){
-
-        specificationUrlDetails = url.parse(specificationLink, true);
-        var get_options = {
-            path: specificationUrlDetails.path,
-            method: 'GET',
-            host: specificationUrlDetails.hostname,
-            port: specificationUrlDetails.port,
-            followRedirect:false
-        };
-        if (!specificationUrlDetails){
-            console.log("ERROR")
-            return;
-        }
-
-        var req = http.get(get_options, function(res) {
-            // Buffer the body entirely for processing as a whole.
-            var bodyChunks = [];
-            var spec = null;
-            res.on('data', function(chunk) {
-                bodyChunks.push(chunk);
-            }).on('end', function() {
-                body = Buffer.concat(bodyChunks);
-                if (body == "NOTHING for you"){
-                    console.log("NO specification ... nothing to do!");
-                }else{
-                    spec = JSON.parse(body);
-                    var dest = spec._params['The destination address']._value;
-                    var reqNum = spec._params['The number of echo requests the pinger should send']._value;
-
-                    async.waterfall([
-                        function(callback){
-                            console.log("------------------------------------------");
-                            console.log("So i should work...")
-                            doAPing(dest, 4 , reqNum , callback);
-                        }
-                    ], function (err, meanRTT) {
-                        console.log("CALCULATED:"+meanRTT);
-                        postResultToSupervisor( spec , meanRTT);
-
-                    });
-
-                }
-            })
+function doATrace(destination , callback){
+    exec(configuration.main.tracerouteExec + " " + configuration.main.tracerouteOptions + " -s " + __MY_IP__ + " " + destination,
+        function (error, stdout, stderr) {
+            var delays = [];
+            if (error || !stdout){
+                callback(new Error("No answer" , null));
+                return;
+            }
+            else{
+                var rows = stdout.split(/\n/);
+                _.each(rows , function(row , index){
+                    var vals = row.split(/[\t\s]+/);
+                    // Simple and stupid check...
+                    if (vals[(vals.length) -1] == 'ms')
+                        delays.push(vals[(vals.length) -2]);
+                });
+                callback(null, delays);
+            }
+            if (error !== null) {
+                callback(error , null);
+            }
         });
-
-        req.on('error', function(e) {
-            console.log('ERROR: ' + e.message);
-        });
-
-    } , period);
-}
-
-function postResultToSupervisor( specification , meanRTT) {
-    var specificationUrlDetails = url.parse(specificationLink, true);
-    var specSplit = specificationUrlDetails.path.split("=");
-    var token = specSplit[1];
-
-    // Create a result obj from the specification
-    var result = new mplane.Result(specification);
-    // Add the result comlumns
-    if (_.isNaN(meanRTT))
-        meanRTT = 0;
-    result.add_result_column("meanRTT" , new mplane.Element("delay.twoway")).setValue({"meanRTT" : meanRTT});
-
-    var post_data = result.to_dict();
-    var post_options = {
-        path: '/result/token='+token,
-        method: 'POST',
-        host: supervisorIP,
-        port: supervisorPort,
-        followRedirect:false,
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': post_data.length
-        }
-    };
-
-    // Set up the request
-    var post_req = http.request(post_options, function(res) {
-        if (res.statusCode == 200){
-            console.log("The supervisor received the result of my work!");
-        }else{
-            console.log("Unable to send result to the supervisor ("+res.statusCode+")");
-        }
-        // Here since it is async
-        console.log("------------------------------------------");
-    });
-
-    //console.log(post_data)
-    // post the data
-    post_req.write(post_data);
-    post_req.end();
-
-    return;
 }
